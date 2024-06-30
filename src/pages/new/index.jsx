63a7422abfba155ff95d45d42cec97556cb6b8b4
João <mcruznq@gmail.com>
2024-06-30 16:07:33 -0300

    removendo tags

--- src/pages/new/index.jsx
+++ src/pages/new/index.jsx
@@ -33,6 +33,10 @@ export function New(){
     setNewTag("");
   }
 
+  function handleRemoveTag(deleted){
+    setTags(prevState => prevState.filter(tag => tag !== deleted));
+  }
+
   return(
     <Container>
 
@@ -81,7 +85,7 @@ export function New(){
                   <NoteItem
                     key={String(index)}
                     value={tag}
-                    onClick={() => {}}
+                    onClick={() => handleRemoveTag(tag)}
                   />  
                 ))
                 
